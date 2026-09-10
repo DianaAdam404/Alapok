ef24d0258bfb20135d39ceef960aee7da8a54669
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-10 11:15:15 +0200

    2.Feladat

---
+++ Valuta/Program.cs
@@ -0,0 +1,12 @@
+﻿using Valuta;
+//peldany parositott
+ValutaValto januar = new ValutaValto();
+januar.Arfolyam = 370;
+Console.WriteLine($"Euro: 100, Árfolyam:{januar.EurorolHuf(100)} Ft");
+
+ValutaValto februar = new ValutaValto();
+februar.Arfolyam = 380;
+Console.WriteLine($"Euro: 100, Árfolyam:{februar.EurorolHuf(100)} Ft");
+
+//nem peldany parositott            static
+Console.WriteLine($"Euro: 100, Árfolyam:{ValutaValto.FixEurorolHuf(100)} Ft");
