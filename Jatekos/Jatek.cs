b457059e1795216e531b9da228233a8161da5cbb
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-10 11:46:11 +0200

    4. feladat

---
+++ Jatekos/Jatek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jatekos
+{
+    public class Jatek
+    {
+        private int elet;
+        public string Nev { get; set; }
+        public int Elet 
+        {
+            get { return elet; }
+
+            set
+            {
+                if (value < 0) elet = 0;
+                else if (value >100) elet = 100;
+                else elet = value;
+            }
+        }
+
+    }
+}
---
+++ Jatekos/Program.cs
@@ -0,0 +1,8 @@
+﻿using Jatekos;
+Jatek jatekos=new Jatek();
+jatekos.Nev = "Dia1";
+jatekos.Elet = 120;
+Console.WriteLine($"Nev: {jatekos.Nev},Elet: {jatekos.Elet}");
+jatekos.Elet = -20;
+Console.WriteLine($"Nev: {jatekos.Nev},Elet: {jatekos.Elet}");
+
