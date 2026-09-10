a0a1c5d884e6e9a4081c3d184dd71fd73a46475d
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-10 10:35:30 +0200

    1.Feladat

--- Auto/Program.cs
+++ Auto/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Auto;
+Autok piros = new Autok();
+piros.Marka = "Toyota";
+piros.Eletkor = 5;
+
+Autok kek = new Autok();
+Console.Write("Kerem az auto markajat: ");
+kek.Marka = Console.ReadLine();
+Console.Write("Kerem az auto eletkorat: ");
+kek.Eletkor = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"1.Peldany: {piros.Marka}, {piros.Eletkor} év.");
+Console.WriteLine($"2.Peldany: {kek.Marka}, {kek.Eletkor} év.");
